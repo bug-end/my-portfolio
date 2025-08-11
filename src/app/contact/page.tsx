cc378da24137e447311d4eeb31d7a7707fa1bed4
lukasz <l.bielecki13@gmail.com>
2025-08-12 00:10:41 +0200

    creating basic html structure for contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,12 @@
 export default function ContactPage() {
-  return <section>CONTACT</section>;
+  return (
+    <section className="mx-auto w-full max-w-7xl px-4 py-8">
+      <h1 className="mb-6 text-4xl font-bold">Let&apos;s connect</h1>
+      <p className="text-xl">
+        I&apos;m always looking for new opportunities and collaborations. If you have any questions or want to work
+        together, feel free to contact me.
+      </p>
+      {/* TODO: add github and linkedin links */}
+    </section>
+  );
 }
