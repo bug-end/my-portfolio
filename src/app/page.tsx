b52d76804e62fafaf2206f21a610050a04f93d23
lukasz <l.bielecki13@gmail.com>
2025-08-11 23:49:55 +0200

    replacing p tags with semantic h1 for better SEO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,13 @@ export default function HomePage() {
     <AnimatedBackground>
       <div className="mx-auto flex w-full max-w-7xl flex-col items-center gap-15 px-4 py-8">
         <div className="text-center">
-          <p className="text-4xl font-bold md:text-5xl">Łukasz Bielecki</p>
-          <p className="bg-gradient-to-r from-purple-700 to-[#4c27f2] bg-clip-text text-7xl font-bold text-transparent transition-colors duration-1500 hover:from-[#4c27f2] hover:to-purple-700 md:text-9xl">
-            frontend dev
-          </p>
+          <h1 className="text-4xl font-bold md:text-5xl">
+            <span>Łukasz Bielecki</span>
+            <br />
+            <span className="bg-gradient-to-r from-purple-700 to-[#4c27f2] bg-clip-text text-7xl font-bold text-transparent transition-colors duration-1500 hover:from-[#4c27f2] hover:to-purple-700 md:text-9xl">
+              frontend dev
+            </span>
+          </h1>
         </div>
         <div className="flex flex-col items-center gap-4">
           <p className="text-center text-2xl font-bold md:text-3xl">Turning ideas into interactive art</p>
