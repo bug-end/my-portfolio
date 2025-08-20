0cbc31ece9c8d725120b1a45c740d7f743143155
lukasz <l.bielecki13@gmail.com>
2025-08-20 20:53:30 +0200

    improving navigation responsiveness

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,31 +1,26 @@
 import Link from 'next/link';
 
-import { ModeToggle } from '@/components/ModeToggle';
-
 export function Nav() {
   return (
-    <nav className="mx-auto flex max-w-7xl items-center justify-between px-4 pt-40 pb-4">
-      {/* <div className="flex items-center gap-4"> */}
-      <Link href="/" className="group text-5xl font-bold">
+    <nav className="mx-auto flex max-w-3xl flex-col justify-between gap-y-2 px-10 pt-10 pb-4 md:flex-row md:items-center md:pt-40 lg:max-w-5xl">
+      <Link href="/" className="group w-fit text-3xl font-bold lg:text-5xl">
         <span className="inline-block text-purple-700 transition-all duration-600 ease-in-out group-hover:rotate-720 group-hover:text-lime-400">
           /
         </span>
         about_me
       </Link>
-      <Link href="/projects" className="group text-5xl font-bold">
+      <Link href="/projects" className="group w-fit text-3xl font-bold lg:text-5xl">
         <span className="inline-block text-purple-700 transition-all duration-600 ease-in-out group-hover:rotate-720 group-hover:text-lime-400">
           /
         </span>
         projects
       </Link>
-      <Link href="/contact" className="group text-5xl font-bold">
+      <Link href="/contact" className="group w-fit text-3xl font-bold lg:text-5xl">
         <span className="inline-block text-purple-700 transition-all duration-600 ease-in-out group-hover:rotate-720 group-hover:text-lime-400">
           /
         </span>
         contact
       </Link>
-      {/* </div> */}
-      {/* <ModeToggle /> */}
     </nav>
   );
 }
