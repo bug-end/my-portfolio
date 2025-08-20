e8ac1e68db76b947b747cb03a175f02d3f1010fb
lukasz <l.bielecki13@gmail.com>
2025-08-20 20:55:22 +0200

    centring text in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
-    <footer className="text-muted-foreground border-t text-sm">
-      <div className="mx-auto max-w-7xl px-4 py-4">
+    <footer className="text-sm">
+      <div className="mx-auto max-w-7xl px-4 py-4 text-center">
         © {new Date().getFullYear()} Łukasz Bielecki. All rights reserved.
       </div>
     </footer>
